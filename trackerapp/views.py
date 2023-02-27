5b15467f5039ca06a8480345ee63aff9ab74efaa
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-02-27 12:59:53 +0500

    Style buttons and add edit functionality

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ExpenseForm
+from .models import Expense
 
 
 def index(request):
@@ -7,5 +8,14 @@ def index(request):
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
+    
+    expenses = Expense.objects.all()
     expense_form = ExpenseForm()
-    return render(request,'trackerapp/index.html',{'expense_form':expense_form})
+    return render(request,'trackerapp/index.html',{'expense_form':expense_form,'expenses':expenses})
+
+
+def edit(request,id):
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+  
+    return render(request,'trackerapp/edit.html',{'expense_form':expense_form})
