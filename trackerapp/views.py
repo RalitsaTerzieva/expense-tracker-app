b9bcfddfd2e8348e25a7b9cbb20b97348aa34a80
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-02 10:33:09 +0500

    Add Expense form

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .forms import ExpenseForm
 
 
 def index(request):
-    return render(request,'trackerapp/index.html')
+    expense_form = ExpenseForm()
+    return render(request,'trackerapp/index.html',{'expense_form':expense_form})
