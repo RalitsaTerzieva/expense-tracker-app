456f78393f98950b7c0328f649bd3a3daeea56b5
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-03 17:23:56 +0500

    Add and register expense model

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    name = models.CharField(max_length=200)
+    amount = models.IntegerField()
+    category = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
