91a720542d2983febcd077a6f75dd17ce0a81dea
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-21 16:18:40 +0500

    Add year, mont and week sum calculation

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
@@ -15,8 +16,29 @@ def index(request):
     
     expenses = Expense.objects.all()
     total_expenses = expenses.aggregate(Sum('amount'))
+    
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
+    
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+    
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+    
     expense_form = ExpenseForm()
-    return render(request,'trackerapp/index.html',{'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses})
+    
+    return render(request,'trackerapp/index.html',
+                  {'expense_form':expense_form,
+                    'expenses':expenses,
+                    'total_expenses':total_expenses,
+                    'yearly_sum':yearly_sum,
+                    'monthly_sum':monthly_sum,
+                    'weekly_sum':weekly_sum
+                    })
 
 
 def edit(request,id):
