65426953d2c5d8cdade0300b5325e59d4c6c2c92
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-21 11:23:18 +0500

    Style form and add save form logic

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -3,5 +3,9 @@ from .forms import ExpenseForm
 
 
 def index(request):
+    if request.method == 'POST':
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
     expense_form = ExpenseForm()
     return render(request,'trackerapp/index.html',{'expense_form':expense_form})
